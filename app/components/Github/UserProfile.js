0ecb184adaa6d756aeabe677311ffb0c1b496ba8
Joe Prempeh <prempehjoe@gmail.com>
2016-01-20 19:52:09 -0800

    Utilizing Stateless Function Components

--- app/components/Github/UserProfile.js
+++ app/components/Github/UserProfile.js
@@ -1,23 +1,21 @@
 import React from 'React'
 
-class UserProfile extends React.Component {
-	render() {
-		return (
-			<div>
-				<ul className="list-group">
-					{this.props.bio.avatar_url && <li className="list-group-item"><img src={this.props.bio.avatar_url} className="img-rounded img-responsive" /></li>}
-					{this.props.bio.name && <li className="list-group-item">Name: {this.props.bio.name}</li>}
-					{this.props.bio.location && <li className="list-group-item">Location: {this.props.bio.location}</li>}
-					{this.props.bio.login && <li className="list-group-item">Username: {this.props.bio.login}</li>}
-					{this.props.bio.email && <li className="list-group-item">Email: {this.props.bio.email}</li>}
-					{this.props.bio.followers && <li className="list-group-item">Followers: {this.props.bio.followers}</li>}
-					{this.props.bio.following && <li className="list-group-item">Following: {this.props.bio.following}</li>}
-					{this.props.bio.public_repos && <li className="list-group-item">Public Repos: {this.props.bio.public_repos}</li>}
-				</ul>
-			</div>
-		)
-	}
-}
+const UserProfile = ({bio}) => {
+	return (
+		<div>
+			<ul className="list-group">
+				{bio.avatar_url && <li className="list-group-item"><img src={bio.avatar_url} className="img-rounded img-responsive" /></li>}
+				{bio.name && <li className="list-group-item">Name: {bio.name}</li>}
+				{bio.location && <li className="list-group-item">Location: {bio.location}</li>}
+				{bio.login && <li className="list-group-item">Username: {bio.login}</li>}
+				{bio.email && <li className="list-group-item">Email: {bio.email}</li>}
+				{bio.followers && <li className="list-group-item">Followers: {bio.followers}</li>}
+				{bio.following && <li className="list-group-item">Following: {bio.following}</li>}
+				{bio.public_repos && <li className="list-group-item">Public Repos: {bio.public_repos}</li>}
+			</ul>
+		</div>
+	)
+};
 
 UserProfile.propTypes = {
 	username: React.PropTypes.string.isRequired,
--- app/components/Main.js
+++ app/components/Main.js
@@ -2,22 +2,19 @@ import React from 'react'
 //var ReactDOM = require('react-dom');
 import SearchGithub from './SearchGithub'
 
-class Main extends React.Component {
-	render() {
-		return (
-			<div className="main-container">
-				<nav className="navbar navbar-default" role="navigation">
-					<div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-						<SearchGithub history={this.props.history} />
-					</div>
-				</nav>
-				<div className="container">
-					{this.props.children}
+const Main = ({history, children}) => {
+	return (
+		<div className="main-container">
+			<nav className="navbar navbar-default" role="navigation">
+				<div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
+					<SearchGithub history={history} />
 				</div>
+			</nav>
+			<div className="container">
+				{children}
 			</div>
-		)
-	}
-}
+		</div>
+	)
+};
 
-//ReactDOM.render(<Main />, document.getElementById('app'));
 export default Main
